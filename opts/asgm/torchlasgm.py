0c86ceeff51c54c8453238cf24a7826af12b61bb
oas <somefuno@oregonsate.edu>
2023-01-20 15:11:36 -0800

    update readme

--- opts/asgm/torchlasgm.py
+++ opts/asgm/torchlasgm.py
@@ -81,7 +81,10 @@ class AGM():
                 errsq =  error.mul(error.conj())
                 errvar, vk = self.vlpf.compute(u=errsq, x=vk, beta=self.beta_n, bmode=10, step=step)
                 gradnorm_den = (errvar.sqrt().add_(self.eps))
-
+                if step_c ==  1:
+                    self.ss_init = (param.max().abs().add(1e-3))/param.nelement()
+                    print(self.ss_init)
+                    
                 ss_k = self.esslpf.compute(
                     u=self.ss_end,x_init=self.ss_init,noise_k=0,beta=self.beta_ss,step=step_c
                     )
